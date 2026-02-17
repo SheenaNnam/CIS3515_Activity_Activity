37e057ca4978c4570960de870e02fbdf76635c20
Sheena Nnam <sheenannam05@gmail.com>
2026-02-17 12:20:51 -0500

    TODO 3: Changing text Size

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
+const val SIZE_KEY = "TBD?"
 class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,6 +12,7 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
+            textSize = intent.getFloatExtra(SIZE_KEY, 22f)
 
         }
 
